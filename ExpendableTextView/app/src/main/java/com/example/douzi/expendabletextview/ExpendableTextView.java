e2d2bf5842cef604ad4ec7df22c76a1103b4e8e3
zhuxiaohui <douzi@zhuxiaohuideMacBook-Pro.local>
2017-04-21 12:02:58 +0800

    no message

--- ExpendableTextView/app/src/main/java/com/example/douzi/expendabletextview/ExpendableTextView.java
+++ ExpendableTextView/app/src/main/java/com/example/douzi/expendabletextview/ExpendableTextView.java
@@ -6,47 +6,85 @@ import android.text.Layout;
 import android.text.StaticLayout;
 import android.text.TextUtils;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.widget.TextView;
 
 /**
  * Created by douzi on 2017/4/19.
+
+     textView.setMaxLines(3);
+     textView.setEllipsize(TextUtils.TruncateAt.END);
+     textView.setWidth(1440);
+     textView.setText("我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下");
+     if (textView.expendable()) {
+         textView.setExpend(true);
+     }
+
  */
 
 public class ExpendableTextView extends TextView {
 
-    private boolean hasInit;
+    public static final int LINES_INVALID = -1;
+    int maxLines = LINES_INVALID; // 保存最大行数
+    int totalLines = LINES_INVALID; // 总行数，每次设置text时计算更新此变量
+    boolean isExpend; // 记录展开收起状态
+    private int width;
 
     public ExpendableTextView(Context context) {
         super(context);
+        init(null);
     }
 
     public ExpendableTextView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        init(attrs);
     }
 
     public ExpendableTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init(attrs);
     }
 
-    int lineVisual = 3; // 最大可见行数
-    int linesTotal = 0; // 展示全部文本需要的行数
-    boolean isExpend = false; // 记录展开收起状态
+    private void init(@Nullable AttributeSet attrs) {
+        if (attrs != null) {
+            maxLines = attrs.getAttributeIntValue("http://schemas.android.com/apk/res/android", "maxLines", -1);
+        } else {
+            maxLines = LINES_INVALID;
+        }
+        isExpend = maxLines == LINES_INVALID;
+    }
+
+    @Override
+    public void setMaxLines(int maxLines) {
+        super.setMaxLines(maxLines);
+        this.maxLines = maxLines;
+        isExpend = maxLines == LINES_INVALID;
+    }
 
     @Override
     public void setText(CharSequence text, BufferType type) {
-        expend(isExpend);
+        processTotalLines(text);
         super.setText(text, type);
-        if (hasInit && text != null) {
-            int width = getWidth();
-            StaticLayout staticLayout = new StaticLayout(text, 0, text.length(), getPaint(), width,
-                    Layout.Alignment.ALIGN_NORMAL, 0, 0, false, TextUtils.TruncateAt.END, 0);
-            linesTotal = staticLayout.getLineCount();
-            expend(isExpend);
-        }
     }
 
+    private void processTotalLines(CharSequence text) {
+        int width = getWidth();
+        if (width <= 0) {
+            width = this.width;
+        }
+        if (width > 0) {
+            StaticLayout staticLayout = new StaticLayout(text, 0, text.length(), getPaint(), width,
+                    Layout.Alignment.ALIGN_NORMAL, 0, 0, false, TextUtils.TruncateAt.END, 0);
+            totalLines = staticLayout.getLineCount();
+       }
+    }
+
+    /**
+     * 具有了width和text时，返回结果才是正确的
+     * @return
+     */
     public boolean expendable() {
-        return linesTotal > lineVisual;
+        return maxLines != LINES_INVALID && totalLines > maxLines;
     }
 
 
@@ -54,27 +92,23 @@ public class ExpendableTextView extends TextView {
         return isExpend;
     }
 
+    public void setWidth(int width) {
+        this.width = width;
+        super.setWidth(width);
+    }
+
     public void setExpend(boolean expend) {
         if (isExpend != expend) {
-            expend(expend);
-            requestLayout();
+            setExpendForce(expend);
         }
     }
 
-    private void expend(boolean expend) {
+    private void setExpendForce(boolean expend) {
         isExpend = expend;
         if (isExpend) {
-            setMaxLines(Integer.MAX_VALUE);
+            super.setMaxLines(Integer.MAX_VALUE);
         } else {
-            setMaxLines(lineVisual);
-        }
-    }
-
-    @Override
-    public void onWindowFocusChanged(boolean hasWindowFocus) {
-        super.onWindowFocusChanged(hasWindowFocus);
-        if (hasWindowFocus && !hasInit) {
-            hasInit = true;
+            super.setMaxLines(maxLines);
         }
     }
 }
--- ExpendableTextView/app/src/main/java/com/example/douzi/expendabletextview/MainActivity.java
+++ ExpendableTextView/app/src/main/java/com/example/douzi/expendabletextview/MainActivity.java
@@ -3,9 +3,13 @@ package com.example.douzi.expendabletextview;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
+import android.text.StaticLayout;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
@@ -56,6 +60,37 @@ public class MainActivity extends Activity {
                 textView.setExpend(false);
             }
         });
+
+        StaticLayout staticLayout = new StaticLayout(textView.getText(), 0, textView.getText().length(), textView.getPaint(), 1440,
+                Layout.Alignment.ALIGN_NORMAL, 0, 0, true, TextUtils.TruncateAt.END, 0);
+        Log.i(getClass().getSimpleName(), "staticLayout.getEllipsisCount(maxLines) : " + staticLayout.getEllipsisCount(3));
+        Log.i(getClass().getSimpleName(), "staticLayout.getEllipsisStart(maxLines) : " + staticLayout.getEllipsisStart(3));
+        Log.i(getClass().getSimpleName(), "staticLayout.getEllipsizedWidth() : " + staticLayout.getEllipsizedWidth());
+        Log.i(getClass().getSimpleName(), "staticLayout.getLineEnd(maxLines) : " + staticLayout.getLineEnd(3));
+        Log.i(getClass().getSimpleName(), "staticLayout.getLineVisibleEnd(maxLines) : " + staticLayout.getLineVisibleEnd(3));
+
+        Log.i(getClass().getSimpleName(), "staticLayout.getEllipsisCount(2) : " + staticLayout.getEllipsisCount(2));
+        Log.i(getClass().getSimpleName(), "staticLayout.getEllipsisStart(2) : " + staticLayout.getEllipsisStart(2));
+        Log.i(getClass().getSimpleName(), "staticLayout.getEllipsizedWidth() : " + staticLayout.getEllipsizedWidth());
+        Log.i(getClass().getSimpleName(), "staticLayout.getLineEnd(2) : " + staticLayout.getLineEnd(2));
+        Log.i(getClass().getSimpleName(), "staticLayout.getLineVisibleEnd(2) : " + staticLayout.getLineVisibleEnd(2));
+
+
+        View contentView = LayoutInflater.from(this).inflate(R.layout.new_text, null);
+        ExpendableTextView textView = (ExpendableTextView) contentView.findViewById(R.id.new_textview);
+        textView.setMaxLines(3);
+        textView.setEllipsize(TextUtils.TruncateAt.END);
+        textView.setWidth(1440);
+        String text = "我来试一下test我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下我来试一下";
+        textView.setText(text);
+        textView.setExpend(false);
+
+        FrameLayout textContainer = (FrameLayout) this.findViewById(R.id.fl_text_container);
+        textContainer.addView(contentView);
+
+
     }
+
+
 
 }
